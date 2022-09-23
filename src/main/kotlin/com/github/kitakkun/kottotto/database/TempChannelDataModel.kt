0c38e4d6bf08014885236dc563d5ce63165b4f59
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-09-23 22:14:59 +0900

    add ResultRow converter in TempChannelDataModel

--- src/main/kotlin/com/github/kitakkun/kottotto/database/TempChannelDataModel.kt
+++ src/main/kotlin/com/github/kitakkun/kottotto/database/TempChannelDataModel.kt
@@ -1,8 +1,20 @@
 package com.github.kitakkun.kottotto.database
 
+import org.jetbrains.exposed.sql.ResultRow
+
 data class TempChannelDataModel(
     val bindingChannelId: Long,
     val roleId: Long,
     val channelId: Long,
     val guildId: Long,
-)
+) {
+    companion object {
+        fun convert(resultRow: ResultRow) : TempChannelDataModel =
+            TempChannelDataModel(
+                bindingChannelId = resultRow[TempChannel.bindChannelId],
+                roleId = resultRow[TempChannel.tempRoleId],
+                channelId = resultRow[TempChannel.tempChannelId],
+                guildId = resultRow[TempChannel.guildId]
+            )
+    }
+}
