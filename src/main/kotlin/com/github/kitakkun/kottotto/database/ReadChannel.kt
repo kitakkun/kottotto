5ea06d06c6fcd23ac65f22f4755dfb8bfda1e197
kitakkun <48154936+kitakkun@users.noreply.github.com>
2023-06-17 22:36:29 +0900

    change ReadChannel's primary key to guildId

--- src/main/kotlin/com/github/kitakkun/kottotto/database/ReadChannel.kt
+++ src/main/kotlin/com/github/kitakkun/kottotto/database/ReadChannel.kt
@@ -4,11 +4,10 @@ import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.Table
 
 object ReadChannel : Table() {
-    private val id = integer("id").autoIncrement()
     val textChannelId: Column<Long> = long("bindChannelId")
     val ownerId: Column<Long> = long("tempChannelId")
     val voiceChannelId: Column<Long> = long("tempRoleId")
     val guildId: Column<Long> = long("guildId")
 
-    override val primaryKey = PrimaryKey(id)
+    override val primaryKey = PrimaryKey(guildId)
 }
